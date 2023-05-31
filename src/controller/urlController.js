67ef8c4dc206f5b322d8abc414eaaaf54b8afc70
ishaan <67547661+ishaan199@users.noreply.github.com>
2023-05-31 15:51:57 +0530

    post url done

---
+++ src/controller/urlController.js
@@ -0,0 +1,42 @@
+const urlModel = require('../model/url');
+const validUrl = require('valid-url');
+const shortId = require('shortId');
+
+
+const isValid = (value) => {
+    if(typeof value === "undefined" || value === null) return false;
+    if(typeof value === "string" && value.trim().length < 1) return false;
+    return true;
+};
+
+const baseUrl = "http://localhost:3000/";
+
+
+//CreateUrl 
+const createUrl = async (req,res) => {
+    try{
+        const longUrl = req.body.longUrl;
+        if(!isValid(longUrl)){
+            return res.status(400).send({status:false, msg:"please enter a link as a value"})
+        };
+        if(!validUrl.isUri(longUrl)){
+            return res.status(404).send({status:false,msg:"please Enter the Valid Url"});
+        };
+
+        let urlCode = shortId.generate();
+        let shortUrl = baseUrl + urlCode;
+
+        let saveData = {longUrl, shortUrl, urlCode};
+        let saveUrl = await urlModel.create(saveData)
+
+        let result =  {
+            longUrl : saveUrl.longUrl,
+            shortUrl : saveUrl.shortUrl,
+            urlCode : saveUrl.urlCode
+        };
+
+        res.status(201).send({status:true,data:result});
+    }catch(error){
+        res.status(500).send({status:false,error:error.message})
+    };
+};
